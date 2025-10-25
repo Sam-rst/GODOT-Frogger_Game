9197f9e381722f4aab2cf60f0eedbb3c3600c47b
Sam-rst <samuel.ressiot@gmail.com>
2025-10-25 17:14:35 +0200

    feat: Ajoute le delta time dans le calcul des déplacements des voitures

--- scenes/cars/car.gd
+++ scenes/cars/car.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 var direction = Vector2.LEFT
-var speed: int = 1
+var speed: int = 200
 
 func _ready() -> void:
 	if position.x < 0:
@@ -10,7 +10,7 @@ func _ready() -> void:
 		
 
 func _process(delta: float) -> void:
-	position += direction * speed
+	position += direction * speed * delta
 
 
 func _on_visible_on_screen_notifier_2d_screen_exited() -> void:

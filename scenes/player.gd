f5e4fb82bb2cd7ae72cf7b248ca400bcb030a9ae
Sam-rst <samuel.ressiot@gmail.com>
2025-10-25 09:42:18 +0200

    feat: Ajoute le projet avec différentes fonctionnalités (déplacements, animations du joueur, collisions avec des obstacles statiques, camera)

---
+++ scenes/player.gd
@@ -0,0 +1,21 @@
+extends CharacterBody2D
+
+
+var direction: Vector2 = Vector2(1,1)
+var speed: int = 100
+
+func _physics_process(delta: float) -> void:
+	direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = direction * speed
+	animation()
+	move_and_slide()
+
+func animation():
+	if direction:
+		$AnimatedSprite2D.flip_h = direction.x > 0
+		if direction.x != 0:
+			$AnimatedSprite2D.animation = 'Left'
+		else:
+			$AnimatedSprite2D.animation = 'Up' if direction.y < 0 else 'Down'
+	else:
+		$AnimatedSprite2D.frame = 0
